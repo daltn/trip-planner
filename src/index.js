a8d100c2ee976c259e3a209fbb1b8130e47b00b4
Dalton Saffe <daltonsaffe@gmail.com>
2018-09-25 17:12:10 -0400

    maps

--- src/index.js
+++ src/index.js
@@ -1,14 +1,13 @@
 const mapboxgl = require('mapbox-gl');
 
-mapboxgl.accessToken = ///API TOKEN
+mapboxgl.accessToken =
+  'pk.eyJ1IjoiZGFsdG4iLCJhIjoiY2ptaTdqbHRrMDFiNDN3cXBtb3BrbDcyYSJ9.KtnZ950CEihNW-agMQEVQA';
 
 const map = new mapboxgl.Map({
-  container: "map",
+  container: 'map',
   center: [-74.009, 40.705],
-  zoom : 12,
-  style: "mapbox://styles/mapbox/streets-v10"
+  zoom: 12,
+  style: 'mapbox://styles/mapbox/streets-v10',
 });
-
-
 
 console.log('Hayyyyyyyy from JS Zadddiiiez');
