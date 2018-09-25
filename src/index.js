2940158029002054f85824cda05f2c9fc4b4760a
Dalton Saffe <daltonsaffe@gmail.com>
2018-09-25 17:02:39 -0400

    mapbox

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
+const mapboxgl = require('mapbox-gl');
+
+mapboxgl.accessToken = ///API TOKEN
+
+const map = new mapboxgl.Map({
+  container: "map",
+  center: [-74.009, 40.705],
+  zoom : 12,
+  style: "mapbox://styles/mapbox/streets-v10"
+});
+
+
+
 console.log('Hayyyyyyyy from JS Zadddiiiez');
